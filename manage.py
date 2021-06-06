ce5cf668e34077a09de6f32b79880873fe5ecf4e
Ali1041 <aliamin588@gmail.com>
2021-06-06 12:02:16 +0500

    name changed

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import dotenv
 def main():
     """Run administrative tasks."""
     # dotenv.load_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KitchenSite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kitchen.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
